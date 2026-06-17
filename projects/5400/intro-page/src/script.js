12aaeafc5875c552ce36d7557b50b32d3eecd908
nicholasDxy <91774659+nicholasDxy@users.noreply.github.com>
2026-06-17 12:23:52 +0800

    unpublish: 5400/intro-page

--- projects/5400/intro-page/src/script.js
+++
@@ -1,8 +0,0 @@
-document.querySelectorAll('.link').forEach((link) => {
-  link.addEventListener('click', () => {
-    link.style.transform = 'scale(0.96)';
-    setTimeout(() => {
-      link.style.transform = '';
-    }, 120);
-  });
-});
